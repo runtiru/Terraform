a24beebffecdefe3f928005e7d5e281611864b78
tiru <tirumalagadiya@gmail.com>
2023-10-27 17:09:42 +0530

    added v1

---
+++ EC2/V1-EC2.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.23.0"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-1"
+}
+
+resource "aws_instance" "demo-server" {
+    ami = "ami-0cbd40f694b804622"
+    instance_type = "t2.micro"
+    key_name = "NCAL"
+}
