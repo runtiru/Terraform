c4e361c8ce66ab75c051b50352ec73d8c78ba8f6
tiru <tirumalagadiya@gmail.com>
2023-10-27 18:08:15 +0530

    added V2

---
+++ EC2/V2-EC2.tf
@@ -0,0 +1,38 @@
+#Provider
+provider "aws" {
+  region = "us-west-1"
+}
+
+#Resources
+resource "aws_instance" "demo-server" {
+  ami           = "ami-0cbd40f694b804622"
+  instance_type = "t2.micro"
+  key_name      = "NCAL"
+}
+
+#Security_groups
+resource "aws_security_group" "demo-sg" {
+  name        = "demo-sg"
+  description = "Allow SSH"
+
+  ingress {
+    description     = "SSH access"
+    from_port       = 22
+    to_port         = 22
+    protocol        = "tcp"
+    cidr_blocks     = ["0.0.0.0/0"]
+    security_groups = ["demo-sg"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = {
+    Name = "ssh-port"
+  }
+}
